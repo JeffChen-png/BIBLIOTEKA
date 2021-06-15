fe20d04433126268652d5c579a3e5a543fa80510
Рустам <medvedrust@mail.ru>
2021-06-15 11:56:49 +0300

    Initial commit

---
+++ main/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Author, Genre, Book, BookInstance
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra =0
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra =0
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    inlines = [BookInline]
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'book', 'status', 'borrower', 'due_back', 'imprint')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back','borrower')
+        }),
+    )
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Genre)
+admin.site.register(BookInstance, BookInstanceAdmin)
---
+++ main/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# Create your views here.
+#стили для определенных приложений
+
+
+from .models import Book, Author, BookInstance, Genre
+
+
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # Метод 'all()' применен по умолчанию.
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    num_genre = Genre.objects.all().count()
+    num_dushi = Book.objects.filter(title__contains='души').count()
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_genre':num_genre, 'num_dushi':num_dushi, 'num_visits':num_visits},
+    )
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """
+    Generic class-based view listing books on loan to current user.
+    """
+    model = BookInstance
+    template_name = 'main/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
